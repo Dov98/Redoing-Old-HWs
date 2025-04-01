33653063af22b1a3423555e06c674ed43c188b31
Dov98 <dovtheawesome@gmail.com>
2025-03-31 21:52:11 -0400

    Homework 1

---
+++ C#/1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Part 1
+            Console.WriteLine("David Ortiz");
+            Console.WriteLine("Yankee Stadium");
+            Console.WriteLine("Bronx, NYC 10451");
+            Console.WriteLine("(718) 293-4300");
+
+            //Part 2
+            Console.WriteLine("   /\\\n" + "  /  \\\n" + " ------\n" + "[ * * ]\n" + "   \"\n" + "   ^");
+
+            //Part 3
+            Console.WriteLine("2 + 2 = " + (2 + 2));
+            Console.WriteLine("3 * 3 = " + 3 * 3);
+            Console.WriteLine("10 / 2 = " + 10 / 2);
+        }
+
+    }
+}
