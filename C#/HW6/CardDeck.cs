a4102db645adb3259391cc943ff0245ee4fb2099
Dov98 <dovtheawesome@gmail.com>
2025-05-13 21:02:28 -0400

    Homework 6

---
+++ C#/HW6/CardDeck.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace HW6
+{
+    internal class CardDeck
+
+    //make 2 arrays, the ranks and the suites. and then add them together.
+    //tell a person to pick a card
+    //ask them if they want to pcik another
+    {
+        static void Main(string[] args)
+        {
+            bool pick = true;
+
+            string[] rank = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+            string[] suite =  { "Hearts", "Clubs", "Diamonds", "Spades" };
+
+            //prints all the cards in a deck
+            for (int i = 0; i < suite.Length; i++)
+            {
+
+                for (int j = 0; j < rank.Length; j++)
+                {
+                    Console.Write(rank[j]);
+                    Console.Write(" of ");
+                    Console.WriteLine(suite[i]);
+                }
+            }
+
+            Console.WriteLine("Now you see that all the cards are in order.");
+            Console.WriteLine("Pick a card, any card.\n");
+
+            //a loop whre you can pick a random card multiple times
+            while (pick == true)
+            {
+
+            Console.Write("You picked a: ");
+
+            Random rand = new Random();
+                
+
+                Console.Write(rank[rand.Next(0,13)]);
+                Console.Write(" of ");
+                Console.WriteLine(suite[rand.Next(0,4)]);
+
+                Console.WriteLine("Do you want to pick again?\nEnter yes or no.");
+                if(Console.ReadLine() == "yes")
+                {
+                    pick = true;
+                }
+                else
+                {
+                    pick = false;
+                }
+
+            }
+        }
+        
+
+    }
+}
---
+++ C#/HW6/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Runtime.Intrinsics.X86;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace HW6
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+
+            string[] names = new string[3];
+            //names[] = { "Danny", "Luke", "Michael" };
+            names[0] = "Danny";
+            names[1] = "Luke";
+            names[2] = "Michael";
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                //grades(names[i]);
+                Console.Write(names[i] + " - ");
+                grades(names);
+                //letterGrade();
+            }
+        }
+
+        static void grades(string[] name) { 
+
+            int[] scores = new int[8];
+            Random rand = new Random();
+
+            //puts score in each array index
+            for (int i = 0; i < scores.Length; i++)
+            {
+
+                scores[i] = rand.Next(60, 100);
+            }
+
+            //prints scores
+            for (int j = 0; j < scores.Length; j++)
+            {
+                Console.Write(scores[j] + " ");
+            }
+
+            int avg = CalculateAverage(scores);
+            Console.Write("Avg: " + avg);
+
+            //Prints out the score
+            Console.Write(" Grade: ");
+            if (avg > 90)
+            {
+                Console.Write('A');
+            }
+            else if (avg > 80)
+            {
+                Console.Write('B');
+            }
+            else if (avg > 70)
+            {
+                Console.Write('C');
+            }
+            else if (avg > 60)
+            {
+                Console.Write('D');
+            }
+
+            Console.WriteLine();
+
+        }
+
+        //calculates average of the scores
+        static int CalculateAverage(int[] avg)
+        {
+
+            int total = 0;
+
+            for(int i =0; i < avg.Length; i++)
+            {
+                total += avg[i];
+            }            
+
+            return total/ avg.Length;
+        }
+
+        //defines the lettergrade based off the average
+        /*static void letterGrade(int avg)
+        {
+            if(avg > 90)
+            {
+                Console.WriteLine('A');
+            }else if (avg > 80)
+            {
+                Console.WriteLine('B');
+            }else if (avg > 70)
+            {
+                Console.WriteLine('C');
+            }else if(avg > 60)
+            {
+                Console.WriteLine('D');
+            }
+        }*/
+    }
+}
