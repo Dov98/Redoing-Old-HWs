5e85dcb6948dac1e73784e3d349ba4a18c79fef8
Dov98 <dovtheawesome@gmail.com>
2025-07-21 17:41:20 -0400

    Homework 12

---
+++ C#/HW12/Student.cs
@@ -0,0 +1,61 @@
+﻿namespace HW12
+{
+    //Creates a student class
+    internal class Student
+    {
+        public string name;
+        public int age;
+        private char grade;
+
+
+        public char TheGrade
+        {
+            get { return grade; }
+            set { grade = value; }
+        }
+
+        //Method to print the student
+        public void Print()
+        {
+            Console.WriteLine($"My name is {name} and I am {age} years old. My class grade is {TheGrade}");
+        }
+    }
+
+    //Creates an address class
+    internal class Address
+    {
+        private string street;
+        private string city;
+        private string state;
+        private string zip;
+
+        //Makes getters/setters to provide controlled accesss to the original code 
+        public string Thestreet
+        {
+            get { return street; }
+            set { street = value; }
+        }
+        public string Thecity
+        {
+            get { return city; }
+            set { city = value; }
+        }
+        public string Thestate
+        {
+            get { return state; }
+            set { state = value; }
+        }
+        public string Thezip
+        {
+            get { return zip; }
+            set { zip = value; }
+        }
+
+        //Method to print the adress
+        public void Print()
+        {
+            Console.WriteLine($"My address is {Thestreet}, {Thecity}, {Thestate}, {Thezip}");
+        }
+
+    }
+}
---
+++ C#/HW12/StudentTester.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12
+{
+    internal class StudentTester
+    {
+        static void Main(string[] args)
+        {
+            //Creates a new instance of student and adds name, age, and grade
+            Student s1 = new Student();
+            s1.name = "John";
+            s1.age = 17;
+            s1.TheGrade = 'C';
+
+            //Prints out the student variables
+            s1.Print();
+
+            //Creates a new instance of the address class and assigns values to the variables
+            Address a1 = new Address();
+            a1.Thestreet = "Main st";
+            a1.Thecity = "Metropolis";
+            a1.Thestate = "NY";
+            a1.Thezip = "10001";
+
+            //Prints out the address class
+            a1.Print();
+
+        }
+    }
+}
